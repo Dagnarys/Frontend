52590ba58096124fe927398fa41cf7e3d9015efd
dagnarys <super.iard@yandex.ru>
2023-12-26 12:04:27 +0300

    Update Breadcrumbs.tsx
    bread

---
+++ src/components/breadcrumbs/Breadcrumbs.tsx
@@ -0,0 +1,82 @@
+import "./Breadcrumbs.sass"
+import { Link, useLocation } from "react-router-dom";
+import {FaChevronRight} from "react-icons/fa6";
+import {FaHome} from "react-icons/fa";
+import {Driver} from "../../Types";
+import {Dispatch} from "react";
+
+const Breadcrumbs = ({ selectedDriver, setSelectedDriver }: { selectedCity:Driver | undefined, setSelectedCity: Dispatch<Driver | undefined> }) => {
+
+    const location = useLocation()
+
+    let currentLink = ''
+
+    const topics: Record<string, string> = {
+        "drivers": "Водители",
+        "insurances":"Страховки",
+        "profile": "Личный кабинет",
+        "draft": "Черновик",
+        "home": "Главная",
+        "login": "Вход",
+        "register": "Регистрация"
+    }
+
+    const resetSelectedDriver = () => setSelectedDriver(undefined)
+
+    const crumbs = location.pathname.split('/').filter(crumb => crumb !== '').map(crumb => {
+
+        currentLink += `/${crumb}`
+
+        if (Object.keys(topics).find(x => x == crumb))
+        {
+            return (
+                <div className={"crumb"} key={crumb}>
+
+                    <Link to={currentLink} onClick={resetSelectedDriver}>
+                        { (topics as never)[crumb] }
+                    </Link>
+
+                    <FaChevronRight className={"chevron-icon"}/>
+
+                </div>
+            )
+        }
+
+        if (currentLink.match(new RegExp('drivers/(d*)')))
+        {
+            return (
+                <div className={"crumb"} key={crumb}>
+
+                    <Link to={currentLink}>
+                        { selectedDriver?.name }
+                    </Link>
+
+                    <FaChevronRight className={"chevron-icon"}/>
+
+                </div>
+            )
+        }
+    });
+
+    return (
+        <div className="breadcrumbs-wrapper">
+            <div className="breadcrumbs">
+
+                <div className="crumb">
+
+                    <Link to={"/drivers"}>
+                        <FaHome className="home-icon" />
+                    </Link>
+
+                    <FaChevronRight className="chevron-icon" />
+
+                </div>
+
+                {crumbs}
+
+            </div>
+        </div>
+    )
+}
+
+export default Breadcrumbs;
